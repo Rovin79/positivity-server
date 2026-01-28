6ee015469dde874daf1bc9d66447dee6f1313560
Rovin79 <108066635+Rovin79@users.noreply.github.com>
2026-01-28 18:46:32 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
+  cors: { origin: "*" },
 });
 
 const onlineUsers = {}; // socket.id -> username
@@ -21,13 +20,22 @@ io.on("connection", (socket) => {
     io.emit("users", Object.values(onlineUsers));
   }
 
+  socket.on("global_message", (data) => {
+    const from = onlineUsers[socket.id];
+    if (!from) return;
+
+    io.emit("global_message", {
+      user: from,
+      text: data.text,
+    });
+  });
+
   socket.on("disconnect", () => {
     console.log("User disconnected:", onlineUsers[socket.id]);
     delete onlineUsers[socket.id];
     io.emit("users", Object.values(onlineUsers));
   });
 });
-
 
 server.listen(3000, () => {
   console.log("Server running on port 3000");
