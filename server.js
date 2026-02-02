e7f718ada9a22d2ee667cc5ac802ec616ca9784b
Rovin79 <108066635+Rovin79@users.noreply.github.com>
2026-02-02 18:48:51 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -1,42 +1,32 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-
-const app = express();
-const server = http.createServer(app);
-
-const io = new Server(server, {
-  cors: { origin: "*" },
-});
-
-const onlineUsers = {}; // socket.id -> username
-
 io.on("connection", (socket) => {
   const username = socket.handshake.query.username;
 
-  if (username) {
-    onlineUsers[socket.id] = username;
-    console.log("User joined:", username);
-    io.emit("users", Object.values(onlineUsers));
-  }
+  if (!username) return;
 
-  socket.on("global_message", (data) => {
+  onlineUsers[socket.id] = username;
+  console.log("User joined:", username);
+
+  io.emit("users", Object.values(onlineUsers));
+
+  // 🔐 PRIVATE MESSAGE
+  socket.on("private_message", ({ to, text }) => {
     const from = onlineUsers[socket.id];
     if (!from) return;
 
-    io.emit("global_message", {
-      user: from,
-      text: data.text,
-    });
+    const targetSocketId = Object.keys(onlineUsers).find(
+      (id) => onlineUsers[id] === to
+    );
+
+    if (targetSocketId) {
+      io.to(targetSocketId).emit("private_message", {
+        from,
+        text,
+      });
+    }
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected:", onlineUsers[socket.id]);
     delete onlineUsers[socket.id];
     io.emit("users", Object.values(onlineUsers));
   });
-});
-
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
 });
